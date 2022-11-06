dbc3deee81d11ac551183ff5327f2def24e77744
NorhanMostafa <nsaleh@youxel.com>
2022-11-07 00:00:37 +0200

    add footer

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,6 @@
 export { TopHeaderComponent } from './top-header/top-header.component';
 export { SvgComponent } from './svg/svg.component';
+export { FooterComponent } from './footer/footer.component';
 
 // modules
 export { ControlsModule } from './controls/controls.module';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SvgComponent, TopHeaderComponent, ControlsModule } from './components';
+import {
+  SvgComponent,
+  TopHeaderComponent,
+  ControlsModule,
+  FooterComponent,
+} from './components';
 import { PipesModule } from './pipes/pipes.module';
 
 const BASEMODULES = [CommonModule];
 
 const MODULES = [ControlsModule];
 
-const COMPONENTS = [TopHeaderComponent, SvgComponent];
+const COMPONENTS = [TopHeaderComponent, SvgComponent, FooterComponent];
 
 const PIPES = [PipesModule];
 
