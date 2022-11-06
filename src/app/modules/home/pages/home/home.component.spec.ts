68927c688c7f1db8d161787dcc4acb601dba53a8
NorhanMostafa <nsaleh@youxel.com>
2022-11-06 20:08:04 +0200

    handle unit test

--- src/app/modules/home/pages/home/home.component.spec.ts
+++ src/app/modules/home/pages/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HomeComponent } from './home.component';
 
@@ -8,9 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
