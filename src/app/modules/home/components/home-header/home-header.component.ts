9786b168ce4eac3140d87a9e2b495ed656109a8d
NorhanMostafa <nsaleh@youxel.com>
2022-11-07 15:14:57 +0200

    add ngrx and add to cart

--- src/app/modules/home/components/home-header/home-header.component.ts
+++ src/app/modules/home/components/home-header/home-header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
-  styleUrls: ['./home-header.component.scss']
+  styleUrls: ['./home-header.component.scss'],
 })
 export class HomeHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  count$: Observable<number>;
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
   }
 
+  ngOnInit(): void {}
 }
